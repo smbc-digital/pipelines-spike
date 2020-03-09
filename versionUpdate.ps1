05d4f5c406a87106862f8775e6b9cd83642b0ab5
Jordan <jordan.burns@stockport.gov.uk>
2020-03-09 16:18:35 +0000

    feat(versionUpdate): Add PowerShell script to increment version based on commit message

---
+++ versionUpdate.ps1
@@ -0,0 +1,66 @@
+﻿$commitMessage = "feat(test): This is a feature"
+$global:version=""
+$global:major=""
+$global:minor=""
+$global:patch=""
+$global:newVersion=""
+
+function getVersion { 
+    $content = Get-Content .\package.json -Raw | ConvertFrom-Json
+    $global:version = $content.version
+
+    $versionSplitted = $version.Split(".")
+
+    $global:major = $versionSplitted[0]
+    $global:minor = $versionSplitted[1]
+    $global:patch = $versionSplitted[2]
+}
+
+function print {
+    Write-Output "Version from json: $version"
+    Write-Output "major: $major"
+    Write-Output "minor: $minor"
+    Write-Output "patch: $patch"
+}
+
+function incrementMajor {
+    $major=[int]$major+1
+    $minor=0
+    $patch=0
+    $newVersion="$major.$minor.$patch"
+
+    Write-Output "When I incrementMajor, the new version is: $newVersion"
+}
+
+function incrementMinor {
+    $minor=[int]$minor+1
+    $patch=0
+    $newVersion="$major.$minor.$patch"
+
+    Write-Output "When I incrementMinor, the new version is: $newVersion"
+}
+
+function incrementPatch {
+    $patch=[int]$patch + 1
+    $newVersion="$major.$minor.$patch"
+
+    Write-Output "When I incrementPatch, the new version is: $newVersion"
+}
+
+function incrementVersion {
+    if($commitMessage.StartsWith("major(")) {
+        incrementMajor
+    }
+    if($commitMessage.StartsWith("feat(")) {
+        incrementMinor
+    }
+    if($commitMessage.StartsWith("fix(")) {
+        incrementPatch
+    }
+}
+
+getVersion
+print
+incrementMajor
+incrementMinor
+incrementPatch
